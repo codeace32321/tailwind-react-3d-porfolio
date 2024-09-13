c08a286b577a8d0a51f09c705cf534e0b99adac1
codeace32321 <jomarneri16@gmail.com>
2024-09-14 00:53:38 +0800

    creating routes for navbar and pages

--- the 3d porfolio/src/App.jsx
+++ the 3d porfolio/src/App.jsx
@@ -1,14 +1,17 @@
 import {Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Home, Project, Contact,About } from './pages';
 
 const App = () => {
   return (
-    <main>
+    <main className='bg-slate-300/20'>
       <Router>
-        Navbar
+        <Navbar/>
         <Routes>
-          <Route path='/home' element={'Home'}/>
-          <Route path='/about' element={'About'}/>
-          <Route path='/project' element={'Project'}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/project' element={<Project/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </Router>
     </main>
---
+++ the 3d porfolio/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white item-center justify-center flex font-bold shadow-md">
+        <p className='bluegradient_text'>Marjo</p>
+        </NavLink>
+        <nav className="flex text-lg gap-7 font-medium">
+          <NavLink to="/home" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            Home
+          </NavLink>
+          <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            About
+          </NavLink>
+          <NavLink to="/contact" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            Contact
+          </NavLink>
+          <NavLink to="/project" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            Project
+          </NavLink>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
